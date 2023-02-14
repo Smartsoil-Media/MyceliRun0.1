be14163148d243a252e7abccffaf1e1aa8d52e2b
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-02-14 18:00:41 +0800

    mobile fixes

--- data.js
+++ data.js
@@ -27,8 +27,6 @@ request.onsuccess = function () {
   const dateIndex = store.index("date_created");
   const bestSpeciesIndex = store.index("amount_harvested_and_species");
 
-  store.put({id: timeStamp, date: "14th of Feb", amount_harvested: 5.5, species: "Oyster", notes: "Humidity was low" })
-  store.put({id: timeStamp, date: "15th of Feb", amount_harvested: 2.5, species: "Shittaki", notes: "Humidity was low" })
 }
 
 document.getElementById("task-form").addEventListener("submit", function(event) {
@@ -42,10 +40,9 @@ document.getElementById("task-form").addEventListener("submit", function(event) 
     const amount_harvested = document.getElementById("amount-harvested").value;
     const species = document.getElementById("species").value;
 
-    console.log(date)
-    console.log(notes)
-    console.log(amount_harvested)
-    console.log(species)
+  
+
+    console.log("DATA LOGGED, YOU FKN BEAUTY")
 
     const harvests = {
       id: timeStamp,
@@ -61,106 +58,39 @@ document.getElementById("task-form").addEventListener("submit", function(event) 
       const store = transaction.objectStore("harvests", {keyPath: "id"});
       const request = store.put(harvests);
 
-      let loggedTextEl = document.getElementById("logged-text");
-      loggedTextEl.innerHTML += ` New form submitted at ${timeStamp} `;
-
-      request.onerror = function(event) {
-        console.log("IndexedDB error: " + event.target.errorCode);
-        loggedTextEl.innerHTML += ` dev has cooked it, sorry :( `;
+    request.onsuccess = function() {
+    const getLatest = store.openCursor(null, 'prev');
+    getLatest.onsuccess = function(event) {
+      const formEl = document.getElementById("task-form")
+      const inputEls = formEl.getElementsByTagName("input");
+      for (let i = 0; i < inputEls.length; i++) {
+        inputEls[i].value = "";
       }
+    
+
+
+    const cursor = event.target.result;
+    const latestResult = cursor.value;
+    const seeFormEl = document.getElementById("see-forms-el");
+    seeFormEl.innerHTML = `
+    <p class="result-text">Date Submitted: <span class="result-test-results"> ${latestResult.date}</span> </p>
+    <p class="result-text">Amount Harvested: <span class="result-test-results"> ${latestResult.amount_harvested} (kg's) </span> </p>
+    <p class="result-text" >Species Farmed: <span class="result-test-results"> ${latestResult.species} </span> </p>
+    <p class="result-text" >Notes taken: <span class="result-test-results"> ${latestResult.notes} </span> </p>
+    
+    
+    `;
+  }
+}
+
+// let loggedTextEl = document.getElementById("logged-text");
+// loggedTextEl.innerHTML += `<p class="result-test"> From Submitted: ${date} </p> `;
+
+request.onerror = function(event) {
+  console.log("IndexedDB error: " + event.target.errorCode);
+  loggedTextEl.innerHTML += ` dev has cooked it, sorry :( `;
+}
     } else {
       console.error("The object store named 'harvests' was not found");
     };
   }})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || { open: function() { throw new Error("IndexedDB not supported"); } };
-
-// // CREATING THE DATA BASES
-
-// var harvestDB = window.indexedDB.open("harvests", 1);
-
-// harvestDB.onupgradeneeded = function(event) {
-//   var db = event.target.result;
-//   var objectStore = db.createObjectStore("harvests", {
-//     keyPath: "id",
-//     autoIncrement: false
-//   });
-
-//   objectStore.createIndex("taskDesc", "taskDesc", { unique: false });
-//   objectStore.createIndex("amountHarvested", "amountHarvested", { unique: false });
-//   objectStore.createIndex("species", "species", { unique: false });
-// };
-
-// harvestDB.onsuccess = function(event) {
-//   var db = event.target.result;
-//   if (db.objectStoreNames.contains("harvests")) {
-//     let loggedTextEl = document.getElementById("logged-text");
-//   }
-// };
-
-// var taskDB = window.indexedDB.open("tasks", 1);
-
-// taskDB.onupgradeneeded = function(event) {
-//   var db = event.target.result;
-//   var objectStore = db.createObjectStore("tasks", { keyPath: "id", autoIncrement: true });
-//   objectStore.createIndex("taskName", "taskName", { unique: false });
-//   objectStore.createIndex("timeSpents", "hrs", { unique: false });
-//   objectStore.createIndex("notes", "idNotes", { unique: false });
-// };
-
-// // END OF CREATING DATA BASES ************
-
-// // ADDING DATA TO DATABASES
-
-// document.getElementById("task-form").addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   var date = new Date(document.getElementById("date-input").value);
-//   var taskDesc = document.getElementById("task-desc").value;
-//   var amountHarvested = document.getElementById("amount-harvested").value;
-//   var species = document.getElementById("species").value;
-
-//   var id = new Date().toISOString();
-//   var harvest = {
-//     id: id,
-//     date: date,
-//     taskDesc: taskDesc,
-//     amountHarvested: amountHarvested,
-//     species: species
-//   };
-
-//   var request = window.indexedDB.open("harvests", 1);
-
-//   request.onsuccess = function(event) {
-//     var db = event.target.result;
-//     if (db.objectStoreNames.contains("harvests")) {
-//       var transaction = db.transaction("harvests", "readwrite");
-//       var objectStore = transaction.objectStore("harvests");
-//       var request = objectStore.add(harvest);
-
-//       let loggedTextEl = document.getElementById("logged-text");
-//       loggedTextEl.innerHTML += ` New form submitted at ${id} `;
-
-//       request.onerror = function(event) {
-//         console.log("IndexedDB error: " + event.target.errorCode);
-//         loggedTextEl.innerHTML += ` dev has cooked it, sorry :( `;
-//       };
-//     } else {
-//       console.error("The object store named 'harvests' was not found");
-//     }
-//   };
-// });
